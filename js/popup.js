2a6155e4fa5fe1a412d46a12b3fc9def455d80ff
Supermaverick <54898325+Supermaverick@users.noreply.github.com>
2019-10-23 12:23:04 +0700

    Подготовка к защите

--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,29 @@
 var link = document.querySelector(".search-caption");
 var popup = document.querySelector(".widget");
-var popupClose = document.querySelector(".close-popup");
-var form = popup.querySelector("form");
 var arrival = popup.querySelector("[name=data-arrival]");
-var departure = popup.querySelector("[name=data-departure]");
-var adult = popup.querySelector("[name=adult]");
-var child = popup.querySelector("[name=child]");
+var form = popup.querySelector("form");
+var validation = popup.querySelectorAll(".validation");
 link.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popup.classList.add("modal-show");
-    arrival.focus()
+    popup.classList.toggle("modal-show");
+    arrival.focus(); 
+});      
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+        }
+    }
 });
-popupClose.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-    popup.classList.remove("modal-error")
-});       
 form.addEventListener("submit", function(evt) {
     for (var i = 0; i < validation.length; i++) {
-        if (!arrival.value || !departure.value || !adult.value || !child.value) {
+        if (!validation[i].value) {
             evt.preventDefault();
             popup.classList.remove("modal-error");
             popup.offsetWidth = popup.offsetWidth;
             popup.classList.add("modal-error");
-            console.log("Заполните пустые поля");
         }
-    }
-})
+    }  
+});
